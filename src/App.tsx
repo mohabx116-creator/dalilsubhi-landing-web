25435be7c780b8bb5d38aa71067350aa42201a6b
Mohab <mohabx116@gmail.com>
2026-06-15 22:41:02 +0300

    Restore full site coming soon page

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,15 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { ScrollToTop } from './components/ScrollToTop';
-import Layout from './components/Layout';
-import HomePage from './pages/HomePage';
 import ComingSoonPage from './pages/ComingSoonPage';
-import ContactPage from './pages/ContactPage';
-import PrivacyPolicyPage from './pages/PrivacyPolicyPage';
-import TermsPage from './pages/TermsPage';
-import NotFoundPage from './pages/NotFoundPage';
 
 export default function App() {
-  const showComingSoon = import.meta.env.VITE_SHOW_COMING_SOON === 'true';
-
   return (
     <BrowserRouter>
       <ScrollToTop />
       <Routes>
-        {showComingSoon ? (
-          <Route path="/" element={<ComingSoonPage />} />
-        ) : (
-          <Route path="/" element={<Layout><HomePage /></Layout>} />
-        )}
-        <Route path="/coming-soon" element={<ComingSoonPage />} />
-        <Route path="/landing-preview" element={<Layout><HomePage /></Layout>} />
-        <Route path="/contact" element={<Layout><ContactPage /></Layout>} />
-        <Route path="/privacy-policy" element={<Layout><PrivacyPolicyPage /></Layout>} />
-        <Route path="/terms" element={<Layout><TermsPage /></Layout>} />
-        <Route path="*" element={<Layout><NotFoundPage /></Layout>} />
+        <Route path="*" element={<ComingSoonPage />} />
       </Routes>
     </BrowserRouter>
   );
 }
+
